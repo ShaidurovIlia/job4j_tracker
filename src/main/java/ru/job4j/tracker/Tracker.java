b60a275fcef1495ea45920ace7f718077557c053
Admin <shaidurov.ilha@yandex.ru>
2021-11-13 13:42:07 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #227496]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,7 +59,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             System.arraycopy(items, index + 1, items,
-                    index, items.length - index - 1);
+                    index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
