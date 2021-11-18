8d7d218f365bd66cb82135d3fb2f2e746a6288c3
Admin <shaidurov.ilha@yandex.ru>
2021-11-18 22:57:17 +0300

    3.2. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.tracker;
-
-public class StartUl {
-    public static void main(String[] args) {
-        Item item = new Item(1, "Roma");
-        System.out.println(item);
-    }
-}
