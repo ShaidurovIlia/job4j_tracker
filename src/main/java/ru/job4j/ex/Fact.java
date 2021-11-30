4b4c221d261a8fede4e09b28dfa1f0ed6cd13371
Admin <shaidurov.ilha@yandex.ru>
2021-11-30 11:56:19 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365 #234039]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class Fact {
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("N should be bigger 0");
+            throw new IllegalArgumentException("N must be equal to or greater than 0");
         }
         int rsl = 1;
         for (int i = 2; i <= n; i++) {
