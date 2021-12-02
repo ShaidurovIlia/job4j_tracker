9813df81998ab556db3ac5f7ac54536ff4fa5be5
Admin <shaidurov.ilha@yandex.ru>
2021-12-02 09:17:03 +0300

    0.4. Пользовательские исключения. [#219367 #234773]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,12 +6,13 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("Element is missing.");
         }
-            return rsl;
+        return rsl;
     }
 
     public static void main(String[] args) throws  ElementNotFoundException {
