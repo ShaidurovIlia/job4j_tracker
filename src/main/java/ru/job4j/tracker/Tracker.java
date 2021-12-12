e109365fd96bf25e80c4b3480cd36f0b1f29da9e
Admin <shaidurov.ilha@yandex.ru>
2021-12-12 21:51:11 +0300

    6. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -35,7 +35,7 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (int index = 0; index < size; index++) {
+        for (int index = 0; index < items.size(); index++) {
             if (items.get(index).getId() == id) {
                 rsl = index;
                 break;
