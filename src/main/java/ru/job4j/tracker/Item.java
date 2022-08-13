2e47220db5a4efa2df0368fb5e7c2d9b9c370b4e
Admin <shaidurov.ilha@yandex.ru>
2022-08-13 23:14:27 +0300

    3. Liquibase. Интеграционные тесты. [#95881]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -6,14 +6,14 @@ import java.util.Objects;
 
 public class Item {
 
+    private static final DateTimeFormatter FORMATTER
+            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
     private int id;
 
     private String name;
 
     private LocalDateTime created = LocalDateTime.now();
-
-    private static final DateTimeFormatter FORMATTER
-            = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
     }
@@ -51,15 +51,22 @@ public class Item {
     @Override
     public String toString() {
         return String.format(
-                "id: %s, name: %s, created: %s", id, name, FORMATTER.format(created));
+                "id: %s, name: %s, created: %s", id, name,
+                FORMATTER.format(created));
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Item item = (Item) o;
-        return id == item.id && Objects.equals(name, item.name) && Objects.equals(created, item.created);
+        return id == item.id
+                && Objects.equals(name, item.name)
+                && Objects.equals(created, item.created);
     }
 
     @Override
